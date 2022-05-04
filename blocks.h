da463f99a38ccae8f5f85bcb5d50d13555eece5a
consoom <dumpdiedump@gmail.com>
2022-05-04 23:36:10 +0200

    Gave blocks distinct update signals

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"",		"s-volume",		0,			10},
-	{"", 		"s-mem",		15,			0},
-	{"", 		"s-time",		1,			0},
-	{"", 		"s-date",		60,			0},
-	{"", 		"s-moon",		18000,			0},
-	{"",		"s-nightfilter",	900,			0},
+	{"", 		"s-mem",		15,			11},
+	{"", 		"s-time",		1,			12},
+	{"", 		"s-date",		60,			13},
+	{"", 		"s-moon",		18000,			14},
+	{"",		"s-nightfilter",	900,			15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
