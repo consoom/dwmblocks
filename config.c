4238db3cdf0f7c84d93aed27b3ac90e3bde7e918
consoom <33983173+consoom@users.noreply.github.com>
2023-04-10 15:24:25 +0200

    Added xrdb colors to statusbar

--- config.c
+++ config.c
@@ -6,12 +6,12 @@
 // clang-format off
 Block blocks[] = {
 	      /*Command*/      /*Update Interval*/  /*Update Signal*/
-    {"s-volume",               0,	            10},
-    {"s-mem",	               15,	            11},
-    {"s-time",	               1,                   12},
-    {"s-date",	               60,                  13},
-    {"s-moon",                 18000,	            14},
-    {"s-nightfilter",          900,                 15},
+    {"echo ^C1^$(s-volume)",   0,	            10},
+    {"echo ^C2^$(s-mem)",      15,	            11},
+    {"echo ^C3^$(s-time)",     1,                   12},
+    {"echo ^C4^$(s-date)",     60,                  13},
+    {"echo ^C1^$(s-moon)",     18000,	            14},
+    {"echo ^C1^$(s-nightfilter)",900,               15},
 };
 // clang-format on
 
